290dc2197a7a788851e8d00c0c0b914aa58caa66
Eric Scott <scottericr@gmail.com>
2024-11-12 13:31:33 -0700

    get working with jetstream s3 object storage.

--- _targets.R
+++ _targets.R
@@ -1,50 +1,7 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
 library(targets)
-library(tarchetypes) 
-library(paws.storage)
-library(crew)
-library(qs)
-
-# Set target options:
+library(targets)
+library(geotargets)
 tar_option_set(
-  packages = c("tibble", "ggplot2", "broom"), # Packages that your targets need for their tasks.
-  format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  controller = crew::crew_controller_local(workers = 2, seconds_idle = 60),
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
   repository = "aws",
   resources = tar_resources(
     aws = tar_resources_aws(
@@ -54,28 +11,16 @@ tar_option_set(
     )
   )
 )
-
-# Run the R scripts in the R/ folder with your custom functions:
-tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
-
-# Replace the target list below with your own:
-tar_plan(
+list(
   tar_target(
-    data,
-    tibble(x = rnorm(100), y = rnorm(100))
+    file,
+    system.file("ex/elev.tif", package = "terra"),
+    format = "file",
+    repository = "local"
   ),
-  tar_target(
-    model,
-    lm(y ~ x, data = data)
-  ),
-  tar_target(
-    plot,
-    plot_model(model)
-  ),
-  tar_target(
-    plot_png,
-    ggsave("fig.png", plot = plot),
-    format = "file"
+  tar_terra_rast(
+    rast_example,
+    terra::rast(file)+0,
+    filetype = "COG"
   )
 )
